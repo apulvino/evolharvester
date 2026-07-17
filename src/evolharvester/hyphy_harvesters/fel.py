a0b394c1862e1d581b12293bebd5e71917cc3a1d
Anthony Pulvino <49889595+apulvino@users.noreply.github.com>
2026-07-17 10:40:10 -0500

    6 cols in original json starting from 0... correcting indexing to account for this

--- src/evolharvester/hyphy_harvesters/fel.py
+++ src/evolharvester/hyphy_harvesters/fel.py
@@ -111,8 +111,8 @@ def harvest_fel(felpath):
             if not isinstance(site, list) or len(site) < 5:
                 continue
             ####accts for pos of site idx, per site stats per harvested row
-            alpha, beta, lrt, pval = site[1], site[2], site[3], site[4]
-
+            #alpha, beta, lrt, pval = site[1], site[2], site[3], site[4]
+            alpha, beta, lrt, pval = site[0], site[1], site[2], site[3]
             #####compute w at site since rates are sep posterior alpha/beta. skip when missing/0/undefine
             omega = beta / alpha if alpha is not None and alpha != 0 else None
 
